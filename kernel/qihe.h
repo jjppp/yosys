90564565699a83c49261d65ea6874705ed0e3b5b
jpwang <jpwang@smail.nju.edu.cn>
2024-07-13 13:21:25 +0800

    Fix ARST logging.

--- kernel/qihe.h
+++ kernel/qihe.h
@@ -4,4 +4,6 @@ namespace qihe
 {
 constexpr bool ENABLE_SRST_PRINT = true;
 constexpr bool ENABLE_CLK_PRINT  = true;
+constexpr bool ENABLE_SRST_VALUE = true;
+constexpr bool ENABLE_ARST_VALUE = true;
 };
